bf9b56b22fc9f5339aa85d8f490cbb3fb7920e2f
segun olowoyeye <segunolowoyeye@gmail.com>
2024-07-01 22:08:25 +0100

    sec commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const currentTime = document.getElementById("currentTime");
+const currentDay = document.getElementById("currentDay");
+const slackEmail = document.getElementById("slackEmail");
+
+
+setInterval(() => {
+  const now = new Date();
+  const utcTime = now.toUTCString();
+  currentTime.innerText = utcTime;
+}, 1000);
+
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const today = new Date();
+const dayIndex = today.getDay();
+currentDay.innerText = days[dayIndex];
+
+
+slackEmail.innerText = "segunolowoyeye@gmail.com";
+
