ed7c365a264911f31f0846387e2ef4a9fc5b201a
segun olowoyeye <segunolowoyeye@gmail.com>
2024-07-02 10:13:00 +0100

    taskonecommit

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const currentTime = document.getElementById("currentTime");
 const currentDay = document.getElementById("currentDay");
 const slackEmail = document.getElementById("slackEmail");
 
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 setInterval(() => {
-  const now = new Date();
-  const utcTime = now.toUTCString();
-  currentTime.innerText = utcTime;
-}, 1000);
+	const now = new Date();
+	const utcTime = new Date(now.getTime() + (60 * 60 * 1000)); // adjust for GMT+1
+	const utcTimeString = utcTime.toUTCString();
+	currentTime.innerText = utcTimeString.slice(17, 25); // extract the time part
+	currentDay.innerText = days[now.getDay()]; // update the current day of the week
+}, 1000); // update every second
 
-
-const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-const today = new Date();
-const dayIndex = today.getDay();
-currentDay.innerText = days[dayIndex];
-
-
-slackEmail.innerText = "segunolowoyeye@gmail.com";
-
-
+slackEmail.innerText = 'segunolowoyeye@gmail.com'
